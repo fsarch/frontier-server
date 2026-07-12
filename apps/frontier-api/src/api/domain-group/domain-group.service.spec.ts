b8c9a6f9c21b34d7e2c2bc025e80b33f9034ebcd
Michael Braun <michael@web4brauns.de>
2026-07-12 19:46:39 +0200

    Add mock repository provider for DomainGroupService tests

--- apps/frontier-api/src/api/domain-group/domain-group.service.spec.ts
+++ apps/frontier-api/src/api/domain-group/domain-group.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DomainGroupService } from './domain-group.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DomainGroup } from '../../database/entities/domain-group.entity.js';
 
 describe('DomainGroupService', () => {
   let service: DomainGroupService;
 
+  const mockDomainGroupRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DomainGroupService],
+      providers: [
+        DomainGroupService,
+        {
+          provide: getRepositoryToken(DomainGroup),
+          useValue: mockDomainGroupRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<DomainGroupService>(DomainGroupService);
