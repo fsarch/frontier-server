456a02c89a3d596060b36c86461357b4f588d9a1
Michael Braun <michael@web4brauns.de>
2026-08-21 08:25:03 +0200

    Fix port

--- apps/frontier-api/src/main.ts
+++ apps/frontier-api/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.useWebSocketAdapter(new WsAdapter(app));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 
 bootstrap();
